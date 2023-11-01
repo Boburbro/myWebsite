71454f379a5b7be8de779a064f4b5ea14d1ed8e4
Boburbro <boburotaboyev0@gmail.com>
2023-11-01 22:27:17 +0500

    v

--- boburbro/lib/main.dart
+++ boburbro/lib/main.dart
@@ -1,5 +1,6 @@
-
+import 'package:boburbro/model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:particles_flutter/particles_flutter.dart';
 
 import 'dart:html' as html;
@@ -12,6 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.aBeeZee().fontFamily
+      ),
       debugShowCheckedModeBanner: false,
       home: MyCustomWidget(),
     );
@@ -19,10 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyCustomWidget extends StatelessWidget {
-  List<Map<String, String>> sities = [
-    {"title": "Modul 1", "subtitle": "Quiz app", "url": ""},
-    {"title": "Modul 2", "subtitle": "USER List app","url": ""},
-  ];
+  final saytlar = Saytlar().list;
 
   @override
   Widget build(BuildContext context) {
@@ -30,47 +31,96 @@ class MyCustomWidget extends StatelessWidget {
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Stack(children: [
-        CircularParticle(
-          width: w,
-          height: h,
-          awayRadius: w / 5,
-          numberOfParticles: 150,
-          speedOfParticles: 1.5,
-          maxParticleSize: 7,
-          particleColor: Colors.white.withOpacity(.7),
-          awayAnimationDuration: Duration(milliseconds: 600),
-          awayAnimationCurve: Curves.easeInOutBack,
-          onTapAnimation: true,
-          isRandSize: true,
-          isRandomColor: false,
-          connectDots: true,
-          enableHover: true,
-          hoverColor: Colors.black,
-          hoverRadius: 90,
-        ),
-        SafeArea(
-            child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: sities.length,
-                  itemBuilder: (ctx, index) {
-                    return ListTile(
-                      leading: Icon(Icons.flutter_dash),
-                      title: Text("${sities[index]['title']}"),
-                      subtitle: Text("${sities[index]['subtitle']}"),
-                      trailing: IconButton(onPressed: (){
-                        // json.context.callMethod('open', ['httsp://github.com']);
-                        // js.context.callMethod('open', ['https://stackoverflow.com/questions/ask']);
-                        html.window.open("httsp://github.com", "_blank");
-                      }, icon: Icon(Icons.paste_sharp),),
-                    );
-                  }),
-            )
-          ],
-        ))
-      ]),
+      body: Stack(
+        children: [
+          CircularParticle(
+            width: w,
+            height: h,
+            awayRadius: w / 5,
+            numberOfParticles: 150,
+            speedOfParticles: 1.5,
+            maxParticleSize: 7,
+            particleColor: Colors.white.withOpacity(.7),
+            awayAnimationDuration: Duration(milliseconds: 600),
+            awayAnimationCurve: Curves.easeInOutBack,
+            onTapAnimation: true,
+            isRandSize: true,
+            isRandomColor: false,
+            connectDots: true,
+            enableHover: true,
+            hoverColor: Colors.black,
+            hoverRadius: 90,
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 500,
+                    height: 700,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                    child: ListView.builder(
+                        itemCount: saytlar.length,
+                        itemBuilder: (ctx, index) {
+                          return ListTile(
+                            leading: Icon(Icons.flutter_dash),
+                            title: Text(saytlar[index].title),
+                            subtitle: Text(saytlar[index].subtitle),
+                            trailing: IconButton(
+                              onPressed: () {
+                                html.window.open(saytlar[index].url, "_self");
+                              },
+                              icon: Icon(Icons.login),
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+              TextButton.icon(
+                onPressed: () {
+                                html.window.open("https://t.me/ITwithBobur", "_self");
+
+                },
+                icon: Icon(Icons.telegram),
+                label: Text("Telegram"),
+              )
+            ],
+          ),
+
+          // Expanded(
+          //   child: ListView.builder(
+          //       itemCount: sities.length,
+          //       itemBuilder: (ctx, index) {
+          //         return Container(
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(20),
+          //               color: Colors.white),
+          //           child: Container(
+          //             padding: EdgeInsets.all(10),
+          //             child: ListTile(
+          //               leading: Icon(Icons.flutter_dash),
+          //               title: Text("${sities[index]['title']}"),
+          //               subtitle: Text("${sities[index]['subtitle']}"),
+          //               trailing: IconButton(
+          //                 onPressed: () {
+          //                   html.window.open("https://github.com", "_self");
+          //                 },
+          //                 icon: Icon(Icons.paste_sharp),
+          //               ),
+          //             ),
+          //           ),
+          //         );
+          //       }),
+          // )
+        ],
+      ),
     );
   }
 }
